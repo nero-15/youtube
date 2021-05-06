f8a8228ba2f63e4a73516b8d99f6643e76dc2708
nero-15 <nero15.dev@gmail.com>
2021-05-06 22:09:24 +0900

    setting params

--- youtube/main.go
+++ youtube/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"path"
@@ -28,7 +29,18 @@ func main() {
 		//https://developers.google.com/youtube/v3/sample_requests
 		//https://developers.google.com/youtube/v3/docs/search/list?apix=true&apix_params=%7B%22part%22%3A%22snippet%22%2C%22q%22%3A%22YouTube%20Data%20API%22%2C%22type%22%3A%22video%22%2C%22videoCaption%22%3A%22closedCaption%22%7D
 
-		return c.String(http.StatusOK, baseURL.Path)
+		// パラメータをセット
+		queryParams := baseURL.Query()
+		queryParams.Set("key", config.Config.APIKey)
+		queryParams.Set("part", "snippet")
+		queryParams.Set("order", "videoCount")
+		queryParams.Set("q", "inter")
+		queryParams.Set("type", "video")
+		baseURL.RawQuery = queryParams.Encode()
+		request, _ := http.Get(baseURL.String())
+		fmt.Println(request)
+
+		return c.String(http.StatusOK, baseURL.String())
 	})
 
 	e.Logger.Fatal(e.Start(config.Config.Port))
